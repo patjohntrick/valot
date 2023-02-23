2ae8198459a456a980981d93e7b47b2034562072
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-02-23 11:01:42 +0800

    add constants

---
+++ constant/Colors.ts
@@ -0,0 +1,10 @@
+export const appColor = {
+  sunburntCyclops: '#FD4556',
+  watermelonRed: '#BD3944',
+  caputMortuum: '#53212B',
+
+  milk: '#FFFBF5',
+
+  black: '#000000',
+  eerieBlack: '#0f1923',
+};
---
+++ constant/Strings.ts
@@ -0,0 +1,21 @@
+export const SCREEN_NAMES = {
+  HOME_SCREEN: 'HomeScreen',
+  AGENTS_SCREEN: 'AgentsScreen',
+  BUNDLES_SCREEN: 'BundlesScreen',
+  TIER_SCREEN: 'TierScreen',
+  MAPS_SCREEN: 'MapsScreen',
+  WEAPONS_SCREEN: 'WeaponsScreen',
+};
+
+export const DRAWER_STRINGS = {
+  AGENTS: 'Agents',
+  HOME: 'Home',
+  BUNDLE: 'Bundle',
+  TIER: 'Tier',
+  MAPS: 'Maps',
+  WEAPONS: 'Weapons',
+};
+
+export const Strings = {
+  LOGO: 'vALORaNT',
+};

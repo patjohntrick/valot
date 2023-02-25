e7dd0e7780999ab17960855b35474f94d5cf5574
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-02-25 19:18:16 +0800

    modify user bar

--- components/StyledUserBar.tsx
+++ components/StyledUserBar.tsx
@@ -1,9 +1,18 @@
 import { TouchableOpacity } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import { removeAgent } from '../redux/slice/AgentSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const StyledUserBar = () => {
+  const dispatch = useDispatch();
+  const agent = useSelector((state: any) => state.agent.agent);
   return (
-    <TouchableOpacity onPress={() => console.log('Hello User')}>
+    <TouchableOpacity
+      onPress={() => {
+        console.log('Hello User', agent);
+        dispatch(removeAgent());
+      }}
+    >
       <Feather name='user' size={24} color='black' />
     </TouchableOpacity>
   );

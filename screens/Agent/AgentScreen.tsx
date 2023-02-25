6c1936cf6a02367182d4107208ad47547bd69bf6
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-02-25 19:18:01 +0800

    WIP Agent Screen

--- screens/Agent/AgentScreen.tsx
+++ screens/Agent/AgentScreen.tsx
@@ -1,5 +1,10 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { StyledLayout, StyledLoader } from '../../components';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+  StyledLayout,
+  StyledLoader,
+  StyledParagraph,
+  StyledSubTitle,
+} from '../../components';
 import { useSelector } from 'react-redux';
 import { appColor } from '../../constant';
 import {
@@ -22,13 +27,50 @@ export const AgentScreen = ({ navigation }: AgentScreenProps) => {
   const { params } = useRoute();
   const { data, error, isLoading } = getAgent(params?.agentId);
 
+  // const {
+  //   displayName,
+  //   background,
+  //   fullPortrait,
+  //   role,
+  //   description,
+  //   abilities,
+  // } = data?.data;
+
   if (isLoading) return <StyledLoader />;
 
   return (
     <StyledLayout>
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Text style={styles.textStyle}>{data.data.displayName}</Text>
-      </TouchableOpacity>
+      <Text style={styles.textStyle}>{data.data.displayName}</Text>
+      <View style={styles.heroContainer}>
+        <Image
+          style={styles.imgBackgroundStyle}
+          source={{ uri: data.data.background }}
+          resizeMode='cover'
+        />
+        <Image
+          style={styles.imgStyle}
+          source={{ uri: data.data.fullPortrait }}
+          resizeMode='contain'
+        />
+      </View>
+      <StyledSubTitle text={data.data.role.displayName} />
+      <StyledParagraph text={data.data.description} />
+      <View style={styles.skillContainer}>
+        <StyledSubTitle text={'Skills'} style={styles.titleStyle} />
+        <View style={styles.skillInnerContainer}>
+          {data.data.abilities.map((ability: any, index: number) => {
+            return (
+              <TouchableOpacity key={index}>
+                <Image
+                  resizeMode='cover'
+                  style={styles.skillImg}
+                  source={{ uri: ability.displayIcon }}
+                />
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </View>
     </StyledLayout>
   );
 };
@@ -36,6 +78,63 @@ export const AgentScreen = ({ navigation }: AgentScreenProps) => {
 const styles = StyleSheet.create({
   container: {},
   textStyle: {
-    color: appColor.milkWhite,
+    color: appColor.sunburntCyclopsRed,
+    textAlign: 'center',
+    fontFamily: 'Oswald-Medium',
+    fontSize: 40,
+    textTransform: 'uppercase',
+    marginTop: -5,
+  },
+  titleStyle: {
+    color: appColor.eerieBlack,
+    textAlign: 'center',
+  },
+  heroContainer: {
+    position: 'relative',
+    // borderWidth: 1,
+    // borderColor: 'red',
+    height: 300,
+  },
+  imgStyle: {
+    width: '100%',
+    // height: '100%',
+    // width: 300,
+    height: 290,
+    position: 'absolute',
+    // bottom: 0,
+    // left: '-5%',
+    // borderWidth: 1,
+    // borderColor: 'red',
+  },
+  imgBackgroundStyle: {
+    width: '100%',
+    // width: 300,
+    height: 300,
+    position: 'absolute',
+    // bottom: 0,
+    // left: '-5%',
+    opacity: 0.1,
+    // borderWidth: 1,
+    // borderColor: 'red',
+  },
+  skillContainer: {
+    backgroundColor: appColor.sunburntCyclopsRed,
+    borderRadius: 5,
+    marginTop: 20,
+    paddingBottom: 15,
+  },
+  skillInnerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  skillImg: {
+    width: 60,
+    height: 60,
+    // borderWidth: 1,
+    // borderColor: appColor.milkWhite,
+    borderRadius: 5,
+    marginHorizontal: 10,
   },
 });

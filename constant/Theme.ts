df5f441bb698df0584eb85d679677a7d6848fbe9
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-02-28 17:05:11 +0800

    Theme

---
+++ constant/Theme.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import { appColor } from './Colors';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: appColor.eerieBlack,
+    paddingTop: 15,
+  },
+});

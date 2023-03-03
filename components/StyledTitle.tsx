294a78f2d1d3b1f1df6d9bbc32417bfff9c6f7fc
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-03-03 13:53:36 +0800

    add style props

--- components/StyledTitle.tsx
+++ components/StyledTitle.tsx
@@ -1,11 +1,13 @@
+import { CSSProperties } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { appColor } from '../constant';
 
 type StyledTitleProps = {
   title: string;
+  style?: CSSProperties;
 };
-export const StyledTitle = ({ title }: StyledTitleProps) => {
-  return <Text style={styles.textStyle}>{title}</Text>;
+export const StyledTitle = ({ title, style }: StyledTitleProps) => {
+  return <Text style={[styles.textStyle, style]}>{title}</Text>;
 };
 
 const styles = StyleSheet.create({

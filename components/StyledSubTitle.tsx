e5a8dafd5a26afa3794eb0152707f4e7207f6a60
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-03-03 10:29:47 +0800

    add numberOfLines props

--- components/StyledSubTitle.tsx
+++ components/StyledSubTitle.tsx
@@ -4,10 +4,19 @@ import { appColor } from '../constant';
 type StyledSubTitleProps = {
   text: string;
   style?: any;
+  numberOfLines?: number;
 };
 
-export const StyledSubTitle = ({ text, style }: StyledSubTitleProps) => {
-  return <Text style={[styles.textStyle, style]}>{text}</Text>;
+export const StyledSubTitle = ({
+  text,
+  style,
+  numberOfLines,
+}: StyledSubTitleProps) => {
+  return (
+    <Text style={[styles.textStyle, style]} numberOfLines={numberOfLines}>
+      {text}
+    </Text>
+  );
 };
 
 const styles = StyleSheet.create({
